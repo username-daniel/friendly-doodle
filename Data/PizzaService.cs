80335697ca22e49f1d442bb6d3ba5e060e3ff602
elite <elite@MacBook-Air-de-Admin0001.local>
2023-12-21 16:30:58 +0100

    the specials

--- Data/PizzaService.cs
+++ Data/PizzaService.cs
@@ -4,15 +4,19 @@ namespace tartine.Data;
 
 public class PizzaService
 {
-    private readonly PizzaStoreContext _context;
-
-    public PizzaService(PizzaStoreContext context)
-    {
-        _context = context;
-    }
-
-    public async Task<PizzaSpecial[]> GetPizzasAsync()
-    {
-        return await _context.Specials.ToArrayAsync();
-    }
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    
+           public PizzaService(IServiceScopeFactory serviceScopeFactory)
+           {
+               _serviceScopeFactory = serviceScopeFactory;
+           }
+    
+           public async Task<PizzaSpecial[]> GetPizzasAsync()
+           {
+               using (var scope = _serviceScopeFactory.CreateScope())
+               {
+                   var context = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+                   return await context.Specials.ToArrayAsync();
+               }
+           }
 }
